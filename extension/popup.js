ec78b996603116748400328b2603dab6ed0a7942
Asatryan Georgy <goasatryan@edu.hse.ru>
2022-03-28 15:23:38 +0300

    Extension added

---
+++ extension/popup.js
@@ -0,0 +1,18 @@
+// Initialize butotn with users's prefered color
+let changeColor = document.getElementById("changeColor");
+
+// When the button is clicked, inject setPageBackgroundColor into current page
+changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
+  });
+});
+
+// The body of this function will be execuetd as a content script inside the
+// current page
+function setPageBackgroundColor() {
+	document.body.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+}
